e252ecc3105037f3151dc799aa24ddfffc693c18
Matthew Healy <matthew.healy@codeclan.com>
2017-06-07 14:01:16 +0100

    Add stub test to GildedRoseLibTests

--- GildedRoseLibTests/GildedRoseLibTests.swift
+++ GildedRoseLibTests/GildedRoseLibTests.swift
@@ -1,36 +1,13 @@
-//
-//  GildedRoseLibTests.swift
-//  GildedRoseLibTests
-//
-//  Created by Matthew on 07/06/2017.
-//
-//
-
 import XCTest
 @testable import GildedRoseLib
 
 class GildedRoseLibTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testFoo() {
+        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
+        let app = GildedRose(items: items);
+        app.updateQuality();
+        XCTAssertEqual("fixme", app.items[0].name);
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
