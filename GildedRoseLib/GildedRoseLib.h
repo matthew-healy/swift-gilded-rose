b789bceb3156a742446d3bf56ca078c55245f860
Matthew Healy <matthew.healy@codeclan.com>
2017-06-07 13:54:57 +0100

    Add GildedRoseLib framework for testability

---
+++ GildedRoseLib/GildedRoseLib.h
@@ -0,0 +1,19 @@
+//
+//  GildedRoseLib.h
+//  GildedRoseLib
+//
+//  Created by Matthew on 07/06/2017.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GildedRoseLib.
+FOUNDATION_EXPORT double GildedRoseLibVersionNumber;
+
+//! Project version string for GildedRoseLib.
+FOUNDATION_EXPORT const unsigned char GildedRoseLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GildedRoseLib/PublicHeader.h>
+
+
