f800a177f0faeaeefed53e70da7cf0d2e2de292c
Matthew Healy <matthew.healy@soundcloud.com>
2017-07-05 09:54:57 +0200

    Add name to modern Item type

--- GildedRoseLib/Item.swift
+++ GildedRoseLib/Item.swift
@@ -1,1 +1,3 @@
-
+struct Item {
+    let name: String
+}
--- GildedRoseLibTests/ItemTests.swift
+++ GildedRoseLibTests/ItemTests.swift
@@ -1,5 +1,16 @@
 import XCTest
+@testable import GildedRoseLib
 
 class ItemTests: XCTestCase {
     
+    func test_name() {
+        let sut = Item(name: "Forbs")
+        XCTAssertEqual("Forbs", sut.name)
+    }
+
+    func test_name_notHardcoded() {
+        let sut = Item(name: "What")
+        XCTAssertEqual("What", sut.name)
+    }
+
 }
