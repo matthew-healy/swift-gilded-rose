5188e340c59d827d9d5fb9125758de816c8bca31
Matthew Healy <matthew.healy@soundcloud.com>
2017-07-05 09:49:21 +0200

    Rename Item class and tests to LegacyItem

---
+++ GildedRoseLib/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  GildedRoseLib
+//
+//  Created by Matthew Healy on 05.07.17.
+//
+
+import Foundation
--- GildedRoseLibTests/LegacyTests/LegacyItemTests.swift
+++ GildedRoseLibTests/LegacyTests/LegacyItemTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import GildedRoseLib
 
-class ItemTests: XCTestCase {
+class LegacyItemTests: XCTestCase {
     
     var sut: Legacy.Item!
     
