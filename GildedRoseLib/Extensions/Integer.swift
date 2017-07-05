056d2d9d9ef6ce7b77711a79e7b784ba78b04f7e
Matthew Healy <matthew.healy@soundcloud.com>
2017-07-05 09:29:41 +0200

    Update to Swift 4

--- GildedRoseLib/Extensions/Integer.swift
+++ GildedRoseLib/Extensions/Integer.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Integer where Stride: SignedInteger {
+extension Comparable where Self: Strideable, Self.Stride: SignedInteger {
     func clamped(to range: CountableClosedRange<Self>) -> Self {
         guard !range.contains(self) else { return self }
         return self > range.upperBound ? range.upperBound : range.lowerBound
